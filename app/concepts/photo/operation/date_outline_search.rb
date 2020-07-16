9a96279cca21cb7caa634f5351dd858e7edc2817
Andrew Delpha <delpha@computer.org>
2020-07-16 01:14:34 -0500

    Added operation to fetch a "date outline" of results.  We can run the normal query and then run this query so we can show the "structure" of the results (prerender X boxes for each of the dates) going back for dates beyond the initial query.
    This will let the user scroll back in time quickly to find the date range they're interested in and once they land, we can query for the photo details in the current date range.

---
+++ app/concepts/photo/operation/date_outline_search.rb
@@ -0,0 +1,67 @@
+require_relative "../contract/search"
+
+class Photo::DateOutlineSearch < Trailblazer::Operation
+  step Model(OpenStruct, :new)
+  step Contract::Build(constant: ::Photo::Contract::Search)
+  step Contract::Validate(key: :search)
+  step :sync_to_model
+  step :extra_sanity_checks
+  step :process_date_params
+  step :search!
+
+  def extra_sanity_checks(options, model:, **)
+    model.page = 0 if model.page.nil? || model.page < 0
+    model.results_per_page = 100 if model.results_per_page.nil? || model.results_per_page > ::Photo::Contract::Search::MAX_RESULTS_PER_PAGE
+    true
+  end
+
+  def process_date_params(options, model:, **)
+    if model.start_date
+      model.start_date = DateTime.iso8601(model.start_date)
+    end
+    if model.end_date
+      model.end_date = DateTime.iso8601(model.end_date)
+    end
+    true
+  end
+
+  def sync_to_model(options, model:, **)
+    options["contract.default"].sync
+    true
+  end
+
+  def search!(options, model:, **)
+    query_chain = ::Photo.group('date(time)')
+    query_chain = query_chain.where(["MATCH(title, description, location_name) AGAINST (?)", model.search_text]) if model.search_text
+    query_chain = query_chain.where(["time >= ?", model.start_date]) if model.start_date
+    query_chain = query_chain.where(["time < ?", model.end_date]) if model.end_date
+    query_chain = query_chain.where(["feature_threshold >= ?", model.min_threshold]) if model.min_threshold
+    query_chain = query_chain.where(["feature_threshold <= ?", model.max_threshold]) if model.max_threshold
+    if model.tags && model.tags.length > 0
+      tag_cnt = 0
+      model.tags.each do |tag_id|
+        tag_id = tag_id.to_i if tag_id.class == String
+        if tag_id
+          query_chain = query_chain.joins("INNER JOIN photo_tags t#{tag_cnt} on photos.id=t#{tag_cnt}.photo_id")
+                            .where(["t#{tag_cnt}.tag_id=?", tag_id])
+        end
+        tag_cnt += 1
+      end
+    end
+    query_chain = query_chain.order(time: :desc)
+    query_chain = query_chain.limit(model.results_per_page)
+    query_chain = query_chain.offset(model.page * model.results_per_page)
+    results_by_date_hash = query_chain.count(:id)
+
+    results_by_date = []
+    results_by_date_hash.each_pair do |d, cnt|
+      results_by_date << {date: sprintf("%04d-%02d-%02d", d.year, d.month, d.day), num_items: cnt}
+    end
+
+    results_by_date = results_by_date.sort { |a, b| b[:date] <=> a[:date] } #reverse date sort new->old
+    options["result_count_by_date"] = results_by_date
+    true
+  end
+
+
+end
---
+++ spec/concepts/photo/operation/date_outline_search_spec.rb
@@ -0,0 +1,119 @@
+require 'rails_helper'
+
+RSpec.describe ::Photo::DateOutlineSearch do
+
+  context "Basic tests" do
+    before :each do
+      op = ::Source::Create.(params: {source: {raw_name: 'unknown', display_name: 'Unknown'}})
+      expect(op).to be_success
+      @source_id = source_id = op[:model].id
+      #manually create a set of a few photos in the database to test on (without creating files)
+      @p1t = DateTime.now - 5.days
+      @p1 = ::Photo.create(:title => "Sunset at Mykonos",
+                           time_id: @p1t.to_i,
+                           time: @p1t,
+                           taken_in_tz: -5 * 60 * 60,
+                           location_name: "Mykonos, GR",
+                           description: "Sunset looking over windmills and the ocean",
+                           source_id: source_id,
+                           metadata: {},
+                           tags: {},
+                           feature_threshold: 1,
+                           image_versions: {}
+      )
+      expect(@p1.id).not_to be_nil
+      @p2t = DateTime.now - 5.years
+      @p2 = ::Photo.create(:title => "Sunset at Grand Canyon",
+                           time_id: @p2t.to_i,
+                           time: @p2t,
+                           taken_in_tz: -5 * 60 * 60,
+                           location_name: "Supai, AZ",
+                           description: "Sunset at Havasu Falls",
+                           source_id: source_id,
+                           metadata: {},
+                           tags: {},
+                           feature_threshold: 2,
+                           image_versions: {}
+      )
+      @p3t = DateTime.now - 2.years
+      @p3 = ::Photo.create(:title => "Golden Gate Bridge",
+                           time_id: @p3t.to_i,
+                           time: @p3t,
+                           taken_in_tz: -5 * 60 * 60,
+                           location_name: "San Francisco, CA",
+                           description: "The Golden Gate Bridge looking out to the ocean",
+                           source_id: source_id,
+                           metadata: {},
+                           tags: {},
+                           feature_threshold: 0,
+                           image_versions: {}
+      )
+      @p4t = DateTime.now - 7.years
+      @p4 = ::Photo.create(:title => "Yosemite Valley",
+                           time_id: @p4t.to_i,
+                           time: @p4t,
+                           taken_in_tz: -5 * 60 * 60,
+                           location_name: "Yosemite National Park",
+                           description: "View from Glacier Point",
+                           source_id: source_id,
+                           metadata: {},
+                           tags: {},
+                           feature_threshold: 0,
+                           image_versions: {}
+      )
+    end
+
+    def date_str(d)
+      sprintf("%04d-%02d-%02d", d.year, d.month, d.day)
+    end
+
+    it "should return correct results for no filter" do
+      result = ::Photo::DateOutlineSearch.(params: {search: {}})
+      expect(result.success?).to be_truthy
+      results_by_date = result["result_count_by_date"]
+      expect(results_by_date).not_to be_nil
+      expect(results_by_date.length).to eq(4)
+      #now check order
+      expect(results_by_date[0][:date]).to eq(date_str(@p1t))
+      expect(results_by_date[0][:num_items]).to eq(1)
+      expect(results_by_date[1][:date]).to eq(date_str(@p3t))
+      expect(results_by_date[1][:num_items]).to eq(1)
+      expect(results_by_date[2][:date]).to eq(date_str(@p2t))
+      expect(results_by_date[2][:num_items]).to eq(1)
+      expect(results_by_date[3][:date]).to eq(date_str(@p4t))
+      expect(results_by_date[3][:num_items]).to eq(1)
+    end
+
+    it "should return correct results for no filter with multiple on a date" do
+      p5t = @p3t + 5.minutes
+      p5 = ::Photo.create(:title => "Golden Gate Bridge At sunset",
+                          time_id: p5t.to_i,
+                          time: p5t,
+                          taken_in_tz: -5 * 60 * 60,
+                          location_name: "San Francisco, CA",
+                          description: "The Golden Gate Bridge looking out to the ocean at sunset",
+                          source_id: @source_id,
+                          metadata: {},
+                          tags: {},
+                          feature_threshold: 0,
+                          image_versions: {}
+      )
+
+
+      result = ::Photo::DateOutlineSearch.(params: {search: {}})
+      expect(result.success?).to be_truthy
+      results_by_date = result["result_count_by_date"]
+      expect(results_by_date).not_to be_nil
+      expect(results_by_date.length).to eq(4)
+      #now check order
+      expect(results_by_date[0][:date]).to eq(date_str(@p1t))
+      expect(results_by_date[0][:num_items]).to eq(1)
+      expect(results_by_date[1][:date]).to eq(date_str(@p3t))
+      expect(results_by_date[1][:num_items]).to eq(2)
+      expect(results_by_date[2][:date]).to eq(date_str(@p2t))
+      expect(results_by_date[2][:num_items]).to eq(1)
+      expect(results_by_date[3][:date]).to eq(date_str(@p4t))
+      expect(results_by_date[3][:num_items]).to eq(1)
+    end
+  end
+end
