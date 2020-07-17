b9bfc4bc94ba524d2abc80fd0264913d1d1d9a7d
Andrew Delpha <delpha@computer.org>
2020-07-17 01:05:01 -0500

    Completely redo rails install to use the API only flag to get rid of all the extra crap for their JS, etc.

--- db/migrate/20200614041449_create_idgentable.rb
+++
@@ -1,6 +0,0 @@
-class CreateIdgentable < ActiveRecord::Migration[6.0]
-  def up
-    execute("create table idgentable(next_value bigint)")
-    execute("insert into idgentable values (1000)") #start it off at 1000.
-  end
-end
