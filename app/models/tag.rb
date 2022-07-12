d157aaacf1b6e6d46af8707842778091570c12cc
Andrew Delpha <andrew@scrible.com>
2022-07-11 19:41:19 -0500

    Added species tag type

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  VALID_TAG_TYPES = [:tag, :location, :people, :event, :album]
+  VALID_TAG_TYPES = [:tag, :location, :people, :event, :album, :species]
   belongs_to :user
   has_many :photo_tags
 end
